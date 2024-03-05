ee43a4f87be50a045c7ae636de9b993a9939a7ad
Oleksandr Nikitin <canon.hanayama@gmail.com>
2024-03-05 16:19:57 +0200

    Added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomePage from './components/Home/HomePage';
-// import './App.css'
+import './App.css'
+import LoginPage from './components/Login/LoginPage';
 
 const App: React.FC = () => {
   return (
     <div id="App">
-      <HomePage />
+      <LoginPage />
     </div>
   );
 };
---
+++ src/components/Login/LoginPage.tsx
@@ -0,0 +1,11 @@
+// import * as React from 'React';
+
+const LoginPage = () => {
+  return (
+    <>
+		  <h1>Login Page</h1>
+	  </>
+  );
+};
+
+export default LoginPage;
